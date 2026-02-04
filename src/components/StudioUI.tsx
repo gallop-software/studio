4816d2b3948573eb3f113ed5b24138fcc1e894df
Chris Baldelomar <chris@webplant.media>
2026-02-04 01:01:20 -0600

    Revert "Always render StudioDetailView to preserve edit modal state"
    This reverts commit 9016ad2b4bd5ef4bf040543fa8f45379d0cf6ef0.

--- src/components/StudioUI.tsx
+++ src/components/StudioUI.tsx
@@ -495,8 +495,8 @@ export function StudioUI({
           </div>
         </div>
 
-        {/* Detail view as modal overlay - always render to preserve edit state */}
-        <StudioDetailView />
+        {/* Detail view as modal overlay */}
+        {focusedItem && <StudioDetailView />}
 
         {/* Error modal */}
         <ErrorModal />
